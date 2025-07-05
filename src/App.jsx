ed75bfe6f629d5e62a7ea2548975052f8984a9c6
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-06 00:25:50 +0100

    added Logout button

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>{user.name}
+        logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}

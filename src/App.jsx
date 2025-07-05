5faf9b6ccaeee2dbe265fe05a2d29422152dc17a
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-06 00:18:18 +0100

    set user to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password }) 
 
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))  
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')

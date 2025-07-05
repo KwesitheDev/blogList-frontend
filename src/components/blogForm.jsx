f66d972961dc74bceab536f6aeefcff135142d99
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-06 00:48:19 +0100

    added blogForm

---
+++ src/components/blogForm.jsx
@@ -0,0 +1,34 @@
+const blogForm = ({ handleSubmit, title, author, url, handleTitleChange, handleAuthorChange, handleUrlChange }) => {
+    return (
+        <form onSubmit={handleSubmit}>
+        <div>
+          title
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          author
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={handleAuthorChange}
+          />
+        </div>
+        <div>
+          url
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={handleUrlChange}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    )
+}

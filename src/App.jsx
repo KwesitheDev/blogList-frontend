a60919e6298c19a5396eed601ffb694125b77607
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-05 04:58:22 +0100

    added handleLogin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from  './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username,setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +15,23 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await loginService.login({ username, password }) 
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error(exception)
+      alert('wrong credentials')
+    }
+  }
+
+
   return (
     <div>
+
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />

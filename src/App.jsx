24648f2e548e51e4ba49c13046f7366788b40e38
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-05 05:07:07 +0100

    conditionally rendered the login form and blogs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from  './services/login'
+import Login from './components/Login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -28,16 +30,28 @@ const App = () => {
     }
   }
 
+  if (user === null) {
+    return (
+      <Login 
+        handleLogin={handleLogin}
+        username={username}
+        password={password}
+        setUsername={setUsername}
+        setPassword={setPassword}
+      />
+    )
+  }
 
   return (
     <div>
-
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
 }
+
 
 export default App

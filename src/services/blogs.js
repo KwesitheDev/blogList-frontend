5e7bd4269d4c6d39eef0ac6abcae1572143289fc
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-06 00:09:43 +0100

    added setToken, create, update methods

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,25 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
+let token = null
+
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject)
+  return response.data
+}
+
+const create = async (newObject) => {
+  const response = await axios.post(baseUrl, newObject)
+  return response.data
+}
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+export default { getAll,setToken,update,create }

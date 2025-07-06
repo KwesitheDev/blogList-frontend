ddec4742fd6c09b57ce5381cd00a49c947c02deb
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-06 01:23:06 +0100

    Notification component

---
+++ src/components/Notification.jsx
@@ -0,0 +1,17 @@
+const Notification = ({ notification }) => {
+  if (!notification) return null;
+
+  const style = {
+    color: notification.type === "error" ? "red" : "green",
+    background: "#f0f0f0",
+    fontSize: 18,
+    border: `2px solid ${notification.type === "error" ? "red" : "green"}`,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  };
+
+  return <div style={style}>{notification.message}</div>;
+};
+
+export default Notification;

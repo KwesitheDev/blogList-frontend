13ae0eb582e675d7018e47b2dccae6c0ca67929a
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-06 01:43:49 +0100

    Notification, Blogform components imported: handleSubmit, handleLogout and other methods added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from  './services/login'
 import Login from './components/Login'
+import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
 
 
 const App = () => {
@@ -10,6 +12,10 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [username,setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title,setTitle] = useState('')
+  const [author,setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -26,6 +32,15 @@ const App = () => {
     }
   }, [])
 
+  //show Notification
+  const showNotification = (message, type = 'success') => {
+  setNotification({ message, type })
+  setTimeout(() => {
+    setNotification(null)
+  }, 5000)
+  }
+
+
   //handle logout 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogAppUser')
@@ -33,6 +48,7 @@ const App = () => {
     blogService.setToken(null)
   }
 
+  //handle login
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
@@ -44,30 +60,72 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exception) {
-      console.error(exception)
-      alert('wrong credentials')
+      showNotification('Wrong credentials', 'error')
     }
   }
 
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value)
+  }
+
+  const handleAuthorChange = (e) => setAuthor(e.target.value)
+
+  const handleTitleChange = (e) => setTitle(e.target.value)
+
+  //handle Submit
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const newBlog = {
+        title, author,url
+      }
+
+      const returnedBlog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(returnedBlog))
+
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      showNotification(`A new blog "${returnedBlog.title}" by ${returnedBlog.author} added`)
+    } catch(exception) {
+      console.log(exception)
+      showNotification('Failed to create blog', 'error')
+    }
+
+  }
+
+
   if (user === null) {
     return (
-      <Login 
+      <div>
+        <Notification notification={notification} />
+
+        <Login 
+        
         handleLogin={handleLogin}
         username={username}
         password={password}
         setUsername={setUsername}
         setPassword={setPassword}
       />
+      </div>
+      
     )
   }
 
   return (
     <div>
+      <Notification notification={notification} />
+
       <h2>blogs</h2>
       <p>{user.name}
         logged in
         <button onClick={handleLogout}>logout</button>
       </p>
+      <BlogForm handleSubmit={handleSubmit} handleAuthorChange={handleAuthorChange}
+        handleTitleChange={handleTitleChange} handleUrlChange={handleUrlChange} 
+        title={title} url={url} author={author}/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}

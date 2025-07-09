fcfeff53e7ec3fcad270196959b5fc5667870671
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-10 00:57:55 +0100

    refactored blogform

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,36 +1,70 @@
-const BlogForm = ({ handleSubmit, title, author, url, handleTitleChange, handleAuthorChange, handleUrlChange }) => {
-    return (
+import { useState } from 'react'
+
+const BlogForm = ({ addBlog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    addBlog({
+      title,
+      author,
+      url
+    })
+
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    setBlogVisible(false)
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setBlogVisible(true)}>create</button>
+      </div>
+
+      <div style={showWhenVisible}>
         <form onSubmit={handleSubmit}>
-        <div>
-          title
-          <input
-            type="text"
-            value={title}
-            name="Title"
-            onChange={handleTitleChange}
-          />
-        </div>
-        <div>
-          author
-          <input
-            type="text"
-            value={author}
-            name="Author"
-            onChange={handleAuthorChange}
-          />
-        </div>
-        <div>
-          url
-          <input
-            type="text"
-            value={url}
-            name="Url"
-            onChange={handleUrlChange}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
-    )
+          <div>
+            title
+            <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url
+            <input
+              type="text"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+        <button onClick={() => setBlogVisible(false)}>cancel</button>
+      </div>
+    </div>
+  )
 }
 
 export default BlogForm

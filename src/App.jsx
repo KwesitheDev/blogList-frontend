7777c5cd6a7adf8698fbd0a02293c95a0a706353
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-10 01:02:59 +0100

    Refactored App

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-import loginService from  './services/login'
+import loginService from './services/login'
 import Login from './components/Login'
 import BlogForm from './components/BlogForm'
 import Notification from './components/Notification'
 
-
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
-  const [username,setUsername] = useState('')
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [title,setTitle] = useState('')
-  const [author,setAuthor] = useState('')
-  const [url, setUrl] = useState('')
   const [notification, setNotification] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => setBlogs(blogs))
   }, [])
 
   useEffect(() => {
@@ -32,29 +26,24 @@ const App = () => {
     }
   }, [])
 
-  //show Notification
   const showNotification = (message, type = 'success') => {
-  setNotification({ message, type })
-  setTimeout(() => {
-    setNotification(null)
-  }, 5000)
+    setNotification({ message, type })
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
   }
 
-
-  //handle logout 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogAppUser')
     setUser(null)
     blogService.setToken(null)
   }
 
-  //handle login
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
-      const user = await loginService.login({ username, password }) 
-
-      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))  
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -64,74 +53,47 @@ const App = () => {
     }
   }
 
-  const handleUrlChange = (e) => {
-    setUrl(e.target.value)
-  }
-
-  const handleAuthorChange = (e) => setAuthor(e.target.value)
-
-  const handleTitleChange = (e) => setTitle(e.target.value)
-
-  //handle Submit
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
+  const addBlog = async (blogObject) => {
     try {
-      const newBlog = {
-        title, author,url
-      }
-
-      const returnedBlog = await blogService.create(newBlog)
+      const returnedBlog = await blogService.create(blogObject)
       setBlogs(blogs.concat(returnedBlog))
-
-      setTitle('')
-      setAuthor('')
-      setUrl('')
       showNotification(`A new blog "${returnedBlog.title}" by ${returnedBlog.author} added`)
-    } catch(exception) {
-      console.log(exception)
+    } catch (exception) {
       showNotification('Failed to create blog', 'error')
     }
-
   }
-
 
   if (user === null) {
     return (
       <div>
         <Notification notification={notification} />
-
-        <Login 
-        
-        handleLogin={handleLogin}
-        username={username}
-        password={password}
-        setUsername={setUsername}
-        setPassword={setPassword}
-      />
+        <Login
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          setUsername={setUsername}
+          setPassword={setPassword}
+        />
       </div>
-      
     )
   }
 
   return (
     <div>
       <Notification notification={notification} />
-
       <h2>blogs</h2>
-      <p>{user.name}
-        logged in
+      <p>
+        {user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
-      <BlogForm handleSubmit={handleSubmit} handleAuthorChange={handleAuthorChange}
-        handleTitleChange={handleTitleChange} handleUrlChange={handleUrlChange} 
-        title={title} url={url} author={author}/>
-      {blogs.map(blog =>
+
+      <BlogForm addBlog={addBlog} />
+
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
-
 
 export default App

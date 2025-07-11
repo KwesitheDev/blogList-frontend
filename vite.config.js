42be3addc9d16815aa910f28e224b45657bb0f9e
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-12 00:24:45 +0100

    test configuration added to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { environments } from 'eslint-plugin-prettier'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js'
   }
 })

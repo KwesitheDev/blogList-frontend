e417d37a04c2e8b8f8b45879444d6baea532b1a5
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-12 04:30:32 +0100

    removed environments

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { environments } from 'eslint-plugin-prettier'
+//import { environments } from 'eslint-plugin-prettier'
 
 // https://vitejs.dev/config/
 export default defineConfig({

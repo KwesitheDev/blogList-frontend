0f8243bbc63c6cbea3854044f835245130de4705
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-12 04:59:05 +0100

    check blog url and number of likes shown onClick of button in blog

--- src/tests/Blog.test.jsx
+++ src/tests/Blog.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 import { expect } from 'vitest'
 
@@ -23,3 +23,27 @@ test('renders blog title and author but not url or likes by default', () => {
   const details = screen.queryByTestId('blog-details')
   expect(details).not.toBeInTheDocument()
 })
+
+test('shows url and likes when the view button is clicked', () => {
+  const blog = {
+    title: 'Testing react Apps',
+    author: 'Kwesithedev',
+    url: 'somelink.com',
+    likes: 7,
+    user: {
+      username: 'test',
+      name: 'Test name'
+    }
+  }
+  render(<Blog blog={blog} />)
+
+  expect(screen.queryByTestId('blog-details')).not.toBeInTheDocument()
+
+  const button = screen.getByTestId('toggle-btn')
+  fireEvent.click(button)
+
+  const details = screen.queryByTestId('blog-details')
+  expect(details).toHaveTextContent(blog.url)
+  expect(details).toHaveTextContent(`likes ${blog.likes}`)
+})
+//used fireEvent instead since i find that more efficient

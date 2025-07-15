2e9c5eed94cc91058f2881599a920be0fb423b5b
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-15 03:56:01 +0100

    fixed import of playwright

--- playwright-tests/tests/blogapp.spec.js
+++ playwright-tests/tests/blogapp.spec.js
@@ -1,17 +1,33 @@
-import {test, describe, beforeEach, expect} from ('@playwright/test')
+import { test, describe, beforeEach, expect } from '@playwright/test'
 
-describe('Blog app', ()=>{
-    beforeEach(
-        async ({ page }) => {
-            await page.goto('http://localhost:5173')
-        }
-    )
+const baseUrl = 'http://localhost:3003'
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post(`${baseUrl}/api/testing/reset`)
 
+    //Create a new User
 
-    test('Login form is shown', async ({ page }) => {
-        await expect(page.getByTestId('login-form')).toBeVisible()  
-        await expect(page.getByTestId('username')).toBeVisible()
-        await expect(page.getByTestId('password')).toBeVisible()
-        await expect(page.getByrole('button',{name:'login'})).toBeVisible()
+    await request.post(`${baseUrl}/api/users`, {
+      data: {
+        name: 'New USer',
+        username: 'newUser',
+        password: 'password'
+      }
     })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByTestId('login-form')).toBeVisible()
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+    await expect(page.getByrole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('Succeeds with right deatails', async ({ page }) => {
+      await page.getByTestId
+    })
+  })
 })

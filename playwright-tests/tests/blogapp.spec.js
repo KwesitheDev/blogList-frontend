9218cbfff6fcd3391ac094c8ddb410376021e945
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-15 03:37:04 +0100

    login form test

--- playwright-tests/tests/blogapp.spec.js
+++ playwright-tests/tests/blogapp.spec.js
@@ -1,1 +1,17 @@
-const {test, describe, beforeEach, expect}
+import {test, describe, beforeEach, expect} from ('@playwright/test')
+
+describe('Blog app', ()=>{
+    beforeEach(
+        async ({ page }) => {
+            await page.goto('http://localhost:5173')
+        }
+    )
+
+
+    test('Login form is shown', async ({ page }) => {
+        await expect(page.getByTestId('login-form')).toBeVisible()  
+        await expect(page.getByTestId('username')).toBeVisible()
+        await expect(page.getByTestId('password')).toBeVisible()
+        await expect(page.getByrole('button',{name:'login'})).toBeVisible()
+    })
+})
